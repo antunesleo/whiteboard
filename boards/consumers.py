68e9e91330731ab0727973e47266ce5f3bfde44e
Leo Antunes <antunesleo4@gmail.com>
2021-08-01 23:35:09 -0300

    Implement logic to draw

--- boards/consumers.py
+++ boards/consumers.py
@@ -19,16 +19,14 @@ class WhiteBoardConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        text_data_json["type"] = "board_message"
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "board_message", "message": message}
+            self.room_group_name, text_data_json
         )
 
     # Receive message from room group
     async def board_message(self, event):
-        message = event["message"]
-
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps(event))
--- boards/static/js/boards.js
+++ boards/static/js/boards.js
@@ -1,3 +1,65 @@
+const boardCanvas = document.getElementById("board_canvas");
+boardCanvas.width = window.innerWidth - 20;
+boardCanvas.height = window.innerHeight - 20;
+
+let canvasContext = boardCanvas.getContext("2d");
+canvasContext.fillStyle="#fff";
+canvasContext.fillRect(0,0, boardCanvas.width, boardCanvas.height);
+
+let isDown = false;
+
+if(boardCanvas){
+    canvasContext.lineWidth = 5;
+
+    boardCanvas.onmousedown = function(e){
+        let message = {
+            event: "on-mouse-down",
+            position: {
+                x: e.pageX - boardCanvas.offsetLeft,
+                y: e.pageY - boardCanvas.offsetTop
+            }
+        }
+        socketConn.send(JSON.stringify(message));
+    }
+
+    boardCanvas.onmousemove = function(e){
+        if(isDown !== false) {
+            let message = {
+                event: "on-mouse-move",
+                position: {
+                    x: e.pageX - boardCanvas.offsetLeft,
+                    y: e.pageY - boardCanvas.offsetTop
+                }
+            }
+            socketConn.send(JSON.stringify(message));
+        }
+    }
+
+    boardCanvas.onmouseup = function(e){
+        let message = {
+            event: "on-mouse-up",
+        }
+        socketConn.send(JSON.stringify(message));
+    }
+}
+
+function handleMouseDown(position) {
+    isDown = true;
+    canvasContext.beginPath();
+    canvasContext.moveTo(position.x, position.y);
+}
+
+function handleMouseMove(position) {
+    canvasContext.lineTo(position.x, position.y);
+    canvasContext.strokeStyle = "#000";
+    canvasContext.stroke();
+}
+
+function handleMouseUp() {
+    isDown = false;
+    canvasContext.closePath();
+}
+
 const boardCode = document.getElementById("board_code").getAttribute("board_code");
 
 const connectionString = 'ws://' + window.location.host + '/ws/boards/' + boardCode + '/';
@@ -10,9 +72,14 @@ socketConn.onclose = function (e) {
 
 socketConn.onmessage = function (e) {
     let data = JSON.parse(e.data);
-    console.log(data);
-};
 
-function ping() {
-    socketConn.send(JSON.stringify({"message": "ping"}));
-}
+    if (data.event === "on-mouse-down") {
+        handleMouseDown(data.position)
+    }
+    else if (data.event === "on-mouse-move") {
+        handleMouseMove(data.position)
+    }
+    else if (data.event === "on-mouse-up") {
+        handleMouseUp()
+    }
+};
